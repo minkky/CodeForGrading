0590c8691a84b3c9f16ab87f4ec2e21fc47208d6
unknown <minjiseo95@gmail.com>
2018-05-18 15:00:26 +0900

    count number add for start

--- rename.py
+++ rename.py
@@ -2,12 +2,13 @@ import os, glob
 
 fpath = "*.py"
 
+count = 1
 for fpath in glob.glob(fpath):
 	
 	name = fpath.split('_')[0]
 	name1 = fpath[-10:-3]
-	filename = name1 + '_' + name + '.py'
+	filename = str(count) + '_' + name1 + '_' + name + '.py'
 	print(filename)
-
+	count = count + 1
 	os.rename(fpath, filename)
 
